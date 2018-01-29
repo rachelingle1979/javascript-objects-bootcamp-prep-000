c1563f67de86e888c89197edec882ba23d17c2f7
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-29 02:50:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artist, song){
   return Object.assign({}, playlist, {[artist]:song});
 }
 
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
